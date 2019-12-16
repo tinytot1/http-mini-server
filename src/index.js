55253a4c8602217f006ac32a4451819ceaa2ba4b
tinytot1 <zh5172@qq.com>
2019-12-16 16:12:05 +0800

    feat(): 完成简易静态服务

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
-// eslint-disable-next-line no-undef
-console.log("-------");
+#!/usr/bin/env node
+
+const koa = require("koa");
+const serveStatic = require("koa-serve-static");
+const app = new koa();
+const root = process.cwd();
+app.use(serveStatic(root));
+
+app.listen(3000, () => {
+  console.log(`serve at http://localhost:3000`);
+});
